9ae94191b5ba835d59cbbda50532199e0227a91e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-09-29 19:31:20 +0200

    update addon builder and fix zip issue

--- PackAddon.py
+++ PackAddon.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from time import time, sleep
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -7,6 +8,13 @@ load_dotenv('.env')
 
 addon_path = str(os.getenv('ADDON_PATH'))
 package_path = str(os.getenv('PACKAGE_PATH'))
+package_holder_path = str(os.getenv('PACKAGE_HOLDER_PATH'))
+
+def createFolder():
+    if os.path.exists(package_holder_path):
+        shutil.rmtree(package_holder_path)
+    os.makedirs(package_path)
+
 
 def packageAddon():
     if not package_path:
@@ -21,14 +29,13 @@ def packageAddon():
         print("Please enter a version")
         return packageAddon()
 
-    dirname = "GuildAssist3"
-    os.mkdir(package_path+dirname)
-
+    createFolder()
+    
     numFiles = 0
     for file in files:
         print("Copy file: " + file + "... ")
         filepath = addon_path + file
-        shutil.copy(filepath, package_path+dirname,  follow_symlinks=True) 
+        shutil.copy(filepath, package_path,  follow_symlinks=True) 
         numFiles += 1
     print(numFiles, " file[s] successfully copied!")
     
@@ -36,21 +43,26 @@ def packageAddon():
     for folder in folders:
         print("Copy directory: " + folder + "...")
         folderpath = addon_path+"\\"+folder+"\\"
-        shutil.copytree(folderpath, package_path+dirname+"\\"+folder)
+        shutil.copytree(folderpath, package_path+"\\"+folder)
         numFolders += 1
     print(numFolders," directory[s] successfully copied!")
 
+    old_path = package_holder_path
     print("Create zip file ...")
-    zipfile = shutil.make_archive(dirname+"_"+version, "zip", root_dir= package_path+dirname)
+    zipfile = shutil.make_archive("GuildAssist3_"+version, "zip", root_dir= package_holder_path)
     print("Move Zip to packages ...")
-    shutil.move(zipfile, package_path)
+    shutil.move(zipfile, addon_path+"\\packages")
+    
     print("Delete temp packaging directory ...")
-    shutil.rmtree(package_path+dirname)
+    shutil.rmtree(old_path)
     print("GuildAssist3_"+version+".zip succesfully created!")
+
     exit(0)
 
 def main():
     packageAddon()
+    #createFolder()
+
 
 if __name__ == '__main__':
     main()
